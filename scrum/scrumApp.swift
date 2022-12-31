db67eb1256921007107b0a3f5625349ab0880ac3
peteykienzzz <pmatczak2@gmail.com>
2022-12-31 10:44:49 -0800

    Initial Commit

---
+++ scrum/scrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumApp.swift
+//  scrum
+//
+//  Created by Piotr Matczak on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct scrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
